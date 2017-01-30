906af3f1bff88228aa486cc4e190ded71d0a0e48
Carolina Grace <cgkenned@live.unc.edu>
2017-01-30 16:29:33 -0500

    First commit.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  data: {
+    itemInputValue: '',
+    cards: [
+      {
+        item: 'finish homework',
+      },
+      {
+        item: 'go on a run'
+      }
+    ]
+  },
+  methods: {
+    addCard () {
+      this.cards.push({
+        content: this.currentText
+      })
+      this.currentText = '';
+    },
+    removeCard (i) {
+      console.log(i);
+    }
+  }
+});
